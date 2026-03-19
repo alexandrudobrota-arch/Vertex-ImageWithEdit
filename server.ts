e42659ca1eb0d62faac17cb91f6e35797bef7565
alexandrudobrota-arch <alexandru.dobrota@bullseyestudio.io>
2026-03-19 13:48:37 +0200

    Add files via upload

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ cloudinary.config({
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: "50mb" }));
 
